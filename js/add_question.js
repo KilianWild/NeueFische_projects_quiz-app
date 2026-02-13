ec5ca769a6b3af1341f1860be8b5b2cb1e23fca7
Kilian <kilian-wild@gmx.de>
2026-02-13 15:46:50 +0100

    create element function complete

--- js/add_question.js
+++ js/add_question.js
@@ -21,6 +21,8 @@ addQuestionForm.addEventListener("submit", (event) => {
 });
 
 function createQuizCard(question, answer) {
+   // <-- create element -->
+   // <--------------------------------------------------------------------------------------------
    const newQuizCard = document.createElement("section");
    newQuizCard.innerHTML = `       
         <section class="quizcard__card" data-js="quizcard__card">
@@ -52,5 +54,44 @@ function createQuizCard(question, answer) {
    newQuestion.textContent = question.value;
    newAnswer.textContent = answer.value;
 
+   // <-- attach quizCard -->
    addQuestionForm.insertAdjacentElement("afterend", newQuizCard);
+
+   // <-- create event listeners -->
+   // <--------------------------------------------------------------------------------------------
+   const questionCardBookmark = newQuizCard.querySelector('[data-js="quizcard__bookmark"]');
+   const questionCardButton_ShowAnswer = newQuizCard.querySelector('[data-js="quizcard__show-answer"]');
+
+   // <-- bookmark -->
+   questionCardBookmark.addEventListener("click", (event) => {
+      const bookmark = event.target.closest('[data-js="quizcard__bookmark"]');
+
+      if (bookmark.dataset.bookmarked === "false") {
+         bookmark.classList.add("bookmarked");
+         bookmark.dataset.bookmarked = true;
+      } else {
+         bookmark.classList.remove("bookmarked");
+         bookmark.dataset.bookmarked = false;
+      }
+   });
+
+   // <-- show answer -->
+   questionCardButton_ShowAnswer.addEventListener("click", (event) => {
+      const buttonShowAnswer = event.target.closest('[data-js="quizcard__show-answer"]');
+      const questionCard = event.target.closest('[data-js="quizcard__card"]');
+      const answerField = questionCard.querySelector('[data-js="answer-field"]');
+      const answerParagraph = questionCard.querySelector('[data-js="answer-paragraph"]');
+
+      if (answerParagraph.classList.contains("is-faded")) {
+         answerParagraph.classList.remove("is-faded");
+         answerField.classList.remove("is-collapsed");
+         answerParagraph.style.transitionDelay = "0.5s";
+         answerField.style.transitionDelay = "0.0s";
+      } else {
+         answerParagraph.classList.add("is-faded");
+         answerField.classList.add("is-collapsed");
+         answerParagraph.style.transitionDelay = "0.0s";
+         answerField.style.transitionDelay = "0.5s";
+      }
+   });
 }
--- js/index.js
+++ js/index.js
@@ -4,7 +4,6 @@ const questionCardButton_ShowAnswer = document.querySelector('[data-js="quizcard
 questionCardBookmark.addEventListener("click", (event) => {
    const bookmark = event.target.closest('[data-js="quizcard__bookmark"]');
 
-   console.log(bookmark.dataset.bookmarked);
    if (bookmark.dataset.bookmarked === "false") {
       bookmark.classList.add("bookmarked");
       bookmark.dataset.bookmarked = true;
